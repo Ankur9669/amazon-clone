7225d5111349b3fdced9531d6cb67b8a8a3d89c3
Ankur9669 <65125998+Ankur9669@users.noreply.github.com>
2021-05-21 12:29:16 +0530

    update: made payment page mobile responsive

--- amazon-clone/src/components/Payment.js
+++ amazon-clone/src/components/Payment.js
@@ -2,10 +2,14 @@ import React from 'react';
 import "../css/payment.css";
 import { useStateValue } from '../StateProvider';
 import Product from "../components/Product";
+import { getBasketTotal } from '../Reducer';
+import SubtotalComponent from './SubtotalComponent';
 function Payment() 
 {
     const [state, dispatch] = useStateValue();
+    let totalItemsInBasket = state.basket?.length;
     let basket = state.basket;
+    let basketTotal = getBasketTotal(state.basket);
     let productCss = {
         product_container_css : "payment-product",
         product_title_css: "payment-product-title",
@@ -26,11 +30,11 @@ function Payment()
                     <h5 className = "customer-name">ankur75@gmail.com</h5>
                     <h5 className = "customer-address">741, kamaniya gate, jabalpur</h5>
                     <h5 className = "customer-country">Bhopal, India</h5>
-                    </div>
                 </div>
+            </div>
 
             <div className = "products-bought-details-container">
-                <h1>Review Items</h1>
+                <h1 className = "products-bought-details-title">Review Items</h1>
                 {
                     basket.map(item => {
                         //console.log(item);
@@ -43,8 +47,14 @@ function Payment()
             </div>
             
             <div className = "payment-method-container">
-                <h1>I am Third Div</h1>
-                <h1>I am sixth Div</h1>
+                <h3 className = "payment-method-label">Payment Method:</h3>
+                <div className = "payment-details-container">
+                    <h3>Card Details</h3>
+                    <div className = "payment-total-price-container">
+                        <h3>Order Total: ${basketTotal}</h3>
+                        <button>Buy Now</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
